167c29052381030be33b7a4d40fcc18e0e641e89
unblockedgames129 <110648595+unblockedgames129@users.noreply.github.com>
2023-03-10 18:25:30 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+ <iframe src="https://replit.com/@roxstar23/Ultimate-Chrome-2-1?embed=1" style="border: 0; width: 100%; height: 100%">Your browser doesn't support iFrames.</iframe>
+`
